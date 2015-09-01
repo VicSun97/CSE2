8be3ec2ad00fd9a8565f0eb8f9de606e8f9f0f4c
vicsun97 <yas219@lehigh.edu>
2015-09-01 16:04:21 +0000

    update WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////
+//Vic Sun
+//hw01
+//WelcomeClass Java Program 
+//
+// Second Compile the Program 
+//        javac WelcomeClass.java 
+// run the program 
+//        java WelcomeClass//
+
+// define a class 
+public class WelcomeClass{
+    
+// add main method 
+  public static void main(String[ ] args) {
+      
+// print the statement with different lines around it and a sentence below the pattern  
+     System.out.println("  ----------- ");
+     System.out.println("  | WELCOME | ");
+     System.out.println("  ----------- ");
+     System.out.println("  ^  ^  ^  ^  ^  ^  ");
+     System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+     System.out.println("<-Y--A--S--2--1--9->");
+     System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+     System.out.println("  v  v  v  v  v  v  ");
+     System.out.println("This August my wife and I took an eleven-day, 675 miles bike");
+     System.out.println("tour on out tandem. We circled lake Ontario.");
+     
+     //test/testtest
+  }
+}
